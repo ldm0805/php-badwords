db09893bb5be773db9d3eb8f30799854fc2f3e76
ldm0805 <leonardodimaggio94@gmail.com>
2023-01-27 14:45:40 +0100

    Modifiche css

--- pcensored.php
+++ pcensored.php
@@ -14,17 +14,28 @@ $censored_string = str_replace($censored,'***', $paragraph);
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <link rel ="stylesheet" href="./css/style.css">
     <title>Primo eserecizio php</title>
 </head>
     <body>
         <div class="center">
-            <h1>
-                <?php echo $paragraph." ".strlen($paragraph);?>
-            </h1>
-            <h1>
-                <?php echo $censored_string." ".strlen($censored_string);?>
-            </h1>
+            <div>
+                <h1>
+                    La tua frase è:
+                </h1>
+                <h1>
+                    <?php echo $paragraph."<br> La tua frase è lunga: ".strlen($paragraph)." caratteri.";?>
+                </h1>
+            </div>
+            <div>
+                <h1>
+                    La tua frase censurata è:
+                </h1>
+                <h1>
+                    <?php echo $censored_string."<br> La tua frase censurata è lunga: ".strlen($censored_string)." caratteri.";?>
+                </h1>
+            </div>
         </div>
     </body>
 </html>
